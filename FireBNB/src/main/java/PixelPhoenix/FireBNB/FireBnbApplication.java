ca43d84c6bacc41e0a6d1d8238a39262d159ca01
liya60188 <lina.yanouri@gmail.com>
2022-04-26 22:05:46 +0200

    First commit

---
+++ FireBNB/src/main/java/PixelPhoenix/FireBNB/FireBnbApplication.java
@@ -0,0 +1,13 @@
+package PixelPhoenix.FireBNB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireBnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireBnbApplication.class, args);
+	}
+
+}
---
+++ FireBNB/src/test/java/PixelPhoenix/FireBNB/FireBnbApplicationTests.java
@@ -0,0 +1,13 @@
+package PixelPhoenix.FireBNB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireBnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
