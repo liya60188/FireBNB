44be2ac6fd20b2668a8422b52dae3b143f2afd64
Juliette <juliette.godinot@eleve.isep.fr>
2022-05-07 22:48:23 +0200

    random changements qui marchent pas

--- FireBNB/src/main/java/PixelPhoenix/FireBNB/controller/HouseController.java
+++ FireBNB/src/main/java/PixelPhoenix/FireBNB/controller/HouseController.java
@@ -4,7 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
 
 import PixelPhoenix.FireBNB.model.House;
 import PixelPhoenix.FireBNB.service.HouseService;
@@ -15,15 +16,24 @@ public class HouseController {
 	@Autowired 
 	private HouseService hssv;
 	
-	@GetMapping("/houses")
+	@GetMapping("/housesList")
 	public String listHouses(Model model) {
 		Iterable<House> listHouses = hssv.getHouses();
 		model.addAttribute("listHouses", listHouses);
 		
 		return "housesList";
 	}
+	/*
 	
-	//@GetMapping("/housePage/{id}")
+	@RequestMapping(value="/housePage/{id}")
+	public String house(Model model,@PathVariable long id) {
+		 Optional<House> house = hssv.getHouse(hssv.getHouse(id));
+		 model.addAttribute("house",house);
+		 
+		 return "house";
+	}
+	
+	*/
 	
 	
 
